c7847875d9df5085906228175b7632f2da4e89c3
reo0306 <reo19840306@gmail.com>
2025-10-21 23:48:57 +0900

    update

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,3 +1,23 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let mut threads = Vec::new();
+
+    for i in 0..8 {
+        let handle = thread::spawn(move || {
+            let result = fibonaci(4000);
+            println!("Thread {} result: {}", i, result);
+        });
+        threads.push(handle);
+    }
+    for handle in threads {
+        handle.join().unwrap();
+    }
+}
+
+fn fibonaci(n: u64) -> u64 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+    fibonaci(n - 1) + fibonaci(n - 2)
 }
